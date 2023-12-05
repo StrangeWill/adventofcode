7200b9247bff26e95b12a7b0e710924cc046c709
William Roush <william.roush@roushtech.net>
2023-12-05 01:26:31 +0000

    Day 1

---
+++ 2023/day-1/part-1/Program.cs
@@ -0,0 +1,15 @@
+﻿var input = await File.ReadAllLinesAsync("../input.txt");
+var sum = input
+    .Select(line =>
+    {
+        var numbers = line
+            .Where(c => int.TryParse(c.ToString(), out var _))
+            .Select(c =>
+            {
+                int.TryParse(c.ToString(), out var num);
+                return num;
+            });
+        return $"{numbers.First()}{numbers.Last()}";
+    })
+    .Sum(int.Parse);
+Console.WriteLine(sum);
---
+++ 2023/day-1/part-2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var input = await File.ReadAllLinesAsync("../input.txt");
+var words = new Dictionary<string, int>()
+{
+    { "one", 1 },
+    { "two", 2 },
+    { "three", 3 },
+    { "four", 4 },
+    { "five", 5 },
+    { "six", 6 },
+    { "seven", 7 },
+    { "eight", 8 },
+    { "nine", 9 },
+    { "zero", 0 }
+};
+var sum = input
+    .Select(line =>
+    {
+        var lineNumbers = new List<int>();
+        for (var i = 0; i < line.Length; i++)
+        {
+            var c = line[i];
+            if (int.TryParse(c.ToString(), out var num))
+            {
+                lineNumbers.Add(num);
+            }
+            else
+            {
+                var number = words.Keys.ToList().Where(s => line.Length - i < s.Length ? false : s == line.Substring(i, s.Length)).FirstOrDefault();
+                if (number != null)
+                {
+                    lineNumbers.Add(words[number]);
+                }
+            }
+
+        }
+
+        return $"{lineNumbers.First()}{lineNumbers.Last()}";
+    })
+    .Sum(int.Parse);
+Console.WriteLine(sum);
